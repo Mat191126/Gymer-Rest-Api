9e4569e94f6383eb1e8f92e5971c32b896c9e256
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-01-25 10:39:10 +0100

    first commit

---
+++ src/main/java/com/gymer/gymer_rest_api/GymerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gymer.gymer_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymerRestApiApplication.class, args);
+	}
+
+}
