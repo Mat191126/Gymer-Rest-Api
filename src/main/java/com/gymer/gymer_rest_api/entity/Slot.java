ee66c6beb4b612716b9e6085378af29b45685f06
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-01-25 13:30:15 +0100

    Add slot Entity and Repository

---
+++ src/main/java/com/gymer/gymer_rest_api/entity/Slot.java
@@ -0,0 +1,32 @@
+package com.gymer.gymer_rest_api.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Slot {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Time startTime;
+	private Time endTime;
+
+	@ManyToOne
+	private Address address;
+	private boolean occupied;
+	private int calendar; //todo change id to object
+
+	public Slot(Time startTime, Time endTime, Address address, boolean occupied, int calendar) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.address = address;
+		this.occupied = occupied;
+		this.calendar = calendar;
+	}
+}
