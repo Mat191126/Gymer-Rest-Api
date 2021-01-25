7c46e68c1586e0231b4fd32eac3defa0acb2e117
Mat191126 <matprz11@gmail.com>
2021-01-25 11:16:37 +0100

    Add Address entity

---
+++ src/main/java/com/gymer/gymer_rest_api/entity/Address.java
@@ -0,0 +1,30 @@
+package com.gymer.gymer_rest_api.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String city;
+    private String street;
+    private int localNumber;
+    private String zipcode;
+
+    public Address(String city, String street, int localNumber, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.localNumber = localNumber;
+        this.zipcode = zipcode;
+    }
+}
