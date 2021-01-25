c1729f954992bb7462febd4aafe3fbc59dd10f1d
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-01-25 13:41:40 +0100

    Add Gym Entity to hold all data about Gym

---
+++ src/main/java/com/gymer/gymer_rest_api/entity/Gym.java
@@ -0,0 +1,51 @@
+package com.gymer.gymer_rest_api.entity;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Gym {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private Time openTime;
+
+    @NotNull
+    private Time closeTime;
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    private Calendar calendar;
+
+    @OneToMany
+    private Iterable<Account> employees;
+
+    public Gym(String name, String description, Time openTime, Time closeTime, Address address, Calendar calendar, Iterable<Account> employees) {
+        this.name = name;
+        this.description = description;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.address = address;
+        this.calendar = calendar;
+        this.employees = employees;
+    }
+
+}
