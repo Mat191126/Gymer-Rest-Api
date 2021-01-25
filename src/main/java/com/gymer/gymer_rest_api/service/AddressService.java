6443911b3f2ce6207c3fac3639528575cb1dc3de
Mat191126 <matprz11@gmail.com>
2021-01-25 11:51:01 +0100

    Add AddresService and ICrudService interface

---
+++ src/main/java/com/gymer/gymer_rest_api/service/AddressService.java
@@ -0,0 +1,47 @@
+package com.gymer.gymer_rest_api.service;
+
+import com.gymer.gymer_rest_api.entity.Address;
+import com.gymer.gymer_rest_api.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AddressService implements ICrudService<Address, Integer> {
+
+    private final AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public Iterable<Address> getAll() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public Optional<Address> get(Integer id) {
+        return addressRepository.findById(id);
+    }
+
+    @Override
+    public void add(Address object) {
+        addressRepository.save(object);
+    }
+
+    @Override
+    public void update(Address object) {
+        Optional<Address> oldAddress = get(object.getId());
+        if (oldAddress.isPresent()) {
+            Address address = oldAddress.get();
+            address.setCity(object.getCity());
+            address.setStreet(object.getStreet());
+            address.setLocalNumber(object.getLocalNumber());
+            address.setZipcode(object.getZipcode());
+            add(address);
+        }
+    }
+}
