f7f088f86ab8a0169c69f15bb0f6f5d1969564c7
Mat191126 <matprz11@gmail.com>
2021-01-28 10:11:44 +0100

    LoggerRequestInterceptor refatcor, delete log4j mail logger, add refactored MailLogger

--- src/main/java/com/gymer/gymer_rest_api/logger/LoggerRequestInterceptor.java
+++ src/main/java/com/gymer/gymer_rest_api/logger/LoggerRequestInterceptor.java
@@ -2,24 +2,31 @@ package com.gymer.gymer_rest_api.logger;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class LoggerRequestInterceptor implements HandlerInterceptor {
 
 	private static final Logger log = LogManager.getRootLogger();
+	private final MailLogger mailLogger = new MailLogger();
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response,
-			Object handler, ModelAndView modelAndView) {
+			Object handler, ModelAndView modelAndView) throws IOException, MessagingException {
 
 		String message = String.format("METHOD: %s, URL: %s, STATUS: %s",
 				request.getMethod(), request.getRequestURI(), response.getStatus());
 		log.info(message);
+		if (!(response.getStatus() == 200)) {
+			mailLogger.sendEmail("mati024211@gmail.com", "Log message", message);
+		}
 	}
 }
