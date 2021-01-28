5e34815bf79b8ba6faec167ed411aa5be9d3471f
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-01-28 11:46:07 +0100

    Fix logging functionality via disable redirecting to /error endpoint

--- src/main/java/com/gymer/gymer_rest_api/logger/LoggerRequestInterceptor.java
+++ src/main/java/com/gymer/gymer_rest_api/logger/LoggerRequestInterceptor.java
@@ -1,10 +1,9 @@
 package com.gymer.gymer_rest_api.logger;
 
-import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,14 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class LoggerRequestInterceptor implements HandlerInterceptor {
 
-	private static final Logger log = LogManager.getRootLogger();
+    // spring.autoconfigure.exclude: org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
+    // this line in application.properties disable redirecting user to /error endpoint
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response,
-			Object handler, ModelAndView modelAndView) {
+    private static final Logger log = LogManager.getRootLogger();
 
-		String message = String.format("METHOD: %s, URL: %s, STATUS: %s",
-				request.getMethod(), request.getRequestURI(), response.getStatus());
-		log.info(message);
-	}
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        String message = String.format("METHOD: %s, URL: %s, STATUS: %s",
+                request.getMethod(), request.getRequestURI(), response.getStatus());
+        log.info(message);
+    }
+
 }
